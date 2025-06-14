0ef62b7ee5148b2e98c1105fdb3eb15399486dd0
Ricardo Oliveira <r200518@dac.unicamp.br>
2025-06-14 13:19:17 -0300

    :construction: Modifica os parâmetros dos métodos para que a classe Biblioteca controle o tempo dos empréstimos

--- app/src/main/java/library/IUser.java
+++ app/src/main/java/library/IUser.java
@@ -3,7 +3,7 @@ package library;
 import java.util.UUID;
 
 public interface IUser {
-    public Emprestimo emprestar(UUID IDObjeto);
-    public Emprestimo renovar(UUID IDObjeto);
+    public Emprestimo emprestar(UUID IDObjeto, int duracaoEmprestimo);
+    public Emprestimo renovar(UUID IDObjeto, int duracaoEmprestimo);
     public Emprestimo devolver(UUID IDObjeto);
 }
