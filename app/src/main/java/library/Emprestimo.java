ea38cdaab36b16328d2bb72941bf49cc5fe93ff0
Ricardo Oliveira <r200518@dac.unicamp.br>
2025-06-14 13:04:02 -0300

    :construction: Adiciona comentários ao record Emprestimo

--- app/src/main/java/library/Emprestimo.java
+++ app/src/main/java/library/Emprestimo.java
@@ -3,6 +3,14 @@ package library;
 import java.time.LocalDate;
 import java.util.UUID;
 
+/**
+ * Record utilizado para representar um emprestimo
+ * @param usuario um UUID que representa o usuario responsavel pelo emprestimo
+ * @param objEmprestado um UUID que representa o objeto emprestado
+ * @param dataEmprestimo a data do emprestimo
+ * @param dataPrevisaoDevolucao a data maxima para devolucao do objeto caso nao haja renovacao
+ * @param dataUltimaRenovacao a data da ultima renovacao feita pelo responsavel pelo emprestimo
+ */
 public record Emprestimo(
     UUID usuario,
     UUID objEmprestado,
