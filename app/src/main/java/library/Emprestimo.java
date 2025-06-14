67f2cc45711b2584b2646dbcb5d3f03c02b22e0d
Ricardo Oliveira <r200518@dac.unicamp.br>
2025-06-14 13:33:45 -0300

    :construction: O email será único então não é necessário ter um UUID para cada usuário

--- app/src/main/java/library/Emprestimo.java
+++ app/src/main/java/library/Emprestimo.java
@@ -5,14 +5,14 @@ import java.util.UUID;
 
 /**
  * Record utilizado para representar um emprestimo
- * @param usuario um UUID que representa o usuario responsavel pelo emprestimo
+ * @param usuario o email do usuario que sera unico
  * @param objEmprestado um UUID que representa o objeto emprestado
  * @param dataEmprestimo a data do emprestimo
  * @param dataPrevisaoDevolucao a data maxima para devolucao do objeto caso nao haja renovacao
  * @param dataUltimaRenovacao a data da ultima renovacao feita pelo responsavel pelo emprestimo
  */
 public record Emprestimo(
-    UUID usuario,
+    String usuario,
     UUID objEmprestado,
     LocalDate dataEmprestimo,
     LocalDate dataPrevisaoDevolucao,
