873ae5c4e3c376621ce2c31c3db05d95f1a4444b
Ricardo Oliveira <r200518@dac.unicamp.br>
2025-06-16 16:26:13 -0300

    :construction: Mudança na implementação dos métodos de User
    Agora User deve apenas gerenciar as referências aos empréstimos gerados
    pela Biblioteca mas toda a criação e exclusão é tarefa de Biblioteca

--- app/src/main/java/library/User.java
+++ app/src/main/java/library/User.java
@@ -1,11 +1,10 @@
 package library;
 
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
-public class User implements IUser{
+
+public class User {
     private String primeiroNome;
     private String email;
     private String senhaHashed;
@@ -29,18 +28,42 @@ public class User implements IUser{
     }
 
 
-    public Emprestimo emprestar(UUID IDObjeto, int duracaoEmprestimo) {
-        LocalDate hoje = LocalDate.now();
-        Emprestimo emprestimo = 
-            new Emprestimo(
-                this.email,
-                IDObjeto,
-                hoje,
-                hoje.plusDays(duracaoEmprestimo),
-                hoje
-            );
-        
+    /**
+     * Utilizado para criar um emprestimo
+     * @param emprestimo o record do emprestimo gerenciado pela Biblioteca
+     */
+    public void emprestar(Emprestimo emprestimo) {
         this.emprestimos.add(emprestimo);
-        return emprestimo;
+    }
+
+
+    /**
+     * Utilizado para remover um emprestimo do sistema ao ser devolvido
+     * Remove o emprestimo com o ID do objeto em emprestimo
+     * @param IDObjeto ID do objeto a ter emprestimo excluido
+     * @return false caso o objeto nao esteja nos emprestimos do usuario
+     */
+    public boolean devolver(String IDObjeto) {
+        for (int i = 0; i < this.emprestimos.size(); ++i) {
+            String objAtual = this.emprestimos.get(i).objEmprestado().toString();
+            if (IDObjeto.equals(objAtual)) {
+                this.emprestimos.remove(i);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+    /**
+     * Utilizado para renovar um emprestimo
+     * @param emprestimo o record do novo emprestimo
+     */
+    public void renovar(Emprestimo emprestimo) {
+        // TODO: Renovações apenas no dia da devolução
+        // Lógica do todo acima deve estar na Biblioteca
+        this.devolver(emprestimo.objEmprestado().toString());
+        this.emprestimos.add(emprestimo);
     }
 }
