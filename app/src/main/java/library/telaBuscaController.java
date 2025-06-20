d942eec1019ef858580631125c5c697c4a14d1d9
marcosrpedroso <m202693@dac.unicamp.br>
2025-06-20 19:32:54 -0300

    atualizações

--- app/src/main/java/library/telaBuscaController.java
+++ app/src/main/java/library/telaBuscaController.java
@@ -4,9 +4,12 @@ import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.util.List;
 import java.util.ArrayList;
