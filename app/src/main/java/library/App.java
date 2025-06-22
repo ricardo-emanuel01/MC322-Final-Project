5357f06da85382cdeb87e317bfb08d18178e8a70
cassiano-caro <cassianocaro2005@gmail.com>
2025-06-22 19:18:59 -0300

    🧁 adicionando arquivos gráficos

--- app/src/main/java/library/App.java
+++ app/src/main/java/library/App.java
@@ -1,6 +1,40 @@
 package library;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-class App {
-    public static void main(String[] args) {}
+public class App extends Application{
+
+    private static Stage mainStage;
+    private static Biblioteca biblioteca;  //Instancia unica da Biblioteca
+
+    public static Biblioteca getBiblioteca() {
+        return biblioteca;
+    }
+
+    public static void main(String[] args) throws Exception {
+        launch(args);
+    }
+
+    public static Stage getMainStage() {
+        return mainStage;
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        mainStage = primaryStage;
+        biblioteca = new Biblioteca("");
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("TelaCriarBiblioteca.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene tela = new Scene(root);
+
+        primaryStage.setTitle("Biblioteca");
+        primaryStage.setScene(tela);
+        primaryStage.setMaximized(true);
+        primaryStage.show();
+    }
 }
